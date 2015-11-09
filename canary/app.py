ba707a3fd3fbaf8fc38504a4b9ed96d7113e0e3b
brian <bolecki019@gmail.com>
2015-11-09 11:55:46 -0500

    added outlier detection using Tukey's method

--- canary/app.py
+++ canary/app.py
@@ -1,5 +1,6 @@
 import falcon
 import skyline
+import outlier
 import backend
 
 api = falcon.API()
@@ -8,12 +9,18 @@ api = falcon.API()
 skyline_MedianAbsoluteDeviation = skyline.MedianAbsoluteDeviation()
 skyline_Grubbs = skyline.Grubbs()
 
+# Outlier Resources
+outlier_Tukey = outlier.Tukey()
+
 # Backend Resources
 backend_AvailableBackends = backend.AvailableBackends()
 
 # Skyline routes
 api.add_route('/v1/algos/skyline/medianabsolutedeviation',skyline_MedianAbsoluteDeviation)
 api.add_route('/v1/algos/skyline/grubbs',skyline_Grubbs)
+
+# Outlier routes
+api.add_route('/v1/algos/outliers/tukey',outlier_Tukey)
 
 # Backend routes
 api.add_route('/v1/backends',backend_AvailableBackends)
---
+++ canary/outlier.py
@@ -0,0 +1,43 @@
+import numpy as np
+import falcon
+
+class Tukey(object):
+    '''
+        A timeseries is anomalous if the last data point is 1.5 times
+        the interquartile range less than the first quartile or more than
+        the third quartile.
+    '''
+
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+    def on_put(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+    def _work(self, timeseries, tidx=0, vidx=1):
+        # Outlier threshold - higher values trigger only extreme outliers
+        threshold = 1.5
+
+        series = np.array([x[vidx] for x in timeseries])
+
+        # Calculate quartiles and IQR
+        q25, q75 = np.percentile(series, [25,75])
+        iqr = q75 - q25
+
+        # Lower and upper outlier boundaries
+        low = q25 - (threshold * iqr)
+        high = q75 + (threshold * iqr)
+
+        # Indexes of outliers
+        low_indexes = np.where(series < low)
+        high_indexes = np.where(series > high)
+
+        indexes = np.concatenate((low_indexes, high_indexes), axis=1)
+
+        length = np.shape(series)[0]
+
+        # Return true if the last index in series is anomalous
+        return np.in1d(length - 1, indexes)[0]
